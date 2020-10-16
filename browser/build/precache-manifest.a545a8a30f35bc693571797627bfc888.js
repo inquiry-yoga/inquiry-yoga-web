d25ae8b4ff97cf47c563cc42a37f2adfce6fdcb1
Ladd Hoffman <ladd.hoffman@alula.net>
2020-10-15 21:51:26 -0500

    including server component

---
+++ browser/build/precache-manifest.a545a8a30f35bc693571797627bfc888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e26b6ca1ed0b5ffcf89460bd0f1278d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa0b1109dbbc880d183",
+    "url": "/static/css/main.e636679e.chunk.css"
+  },
+  {
+    "revision": "d9cb9b6502616ac5904d",
+    "url": "/static/js/2.f8b9e17e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8b9e17e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa0b1109dbbc880d183",
+    "url": "/static/js/main.4b3b26a1.chunk.js"
+  },
+  {
+    "revision": "347d6d13ea7102a26f00",
+    "url": "/static/js/runtime-main.1f7cfae8.js"
+  }
+]);
---
+++ server/index.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const baseWebUri = process.env.BASE_WEB_URI;
+const stripeKey = process.env.STRIPE_KEY;
+
+const stripe = require('stripe')(stripeKey);
+const express = require('express');
+const app = express();
+const winston = require('winston');
+const expressWinston = require('express-winston');
+const debug = require('debug')('app');
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+  colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+  ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
+}));
+
+const YOUR_DOMAIN = baseWebUri + '/checkout';
+
+app.post('/create-session', async (req, res) => {
+  debug('POST');
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Stubborn Attachments',
+            images: ['https://i.imgur.com/EHyR2nP.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.json({ id: session.id });
+});
+
+app.use(express.static('../browser/build'));
+
+app.listen(4242, () => console.log('Running on port 4242'));
